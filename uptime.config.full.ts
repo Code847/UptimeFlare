07f8c81d06b9494e11c2dc006335fe5619d8cf86
代号847 <42256643+Code847@users.noreply.github.com>
2026-02-26 15:07:38 +0800

    Update uptime.config.full.ts

--- uptime.config.full.ts
+++ uptime.config.full.ts
@@ -5,9 +5,9 @@ const pageConfig: PageConfig = {
   title: "lyc8503's Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
+    { link: 'https://github.com/Code847', label: 'GitHub' },
+    { link: 'https://www.btmo.cn', label: 'Blog' },
+    { link: 'mailto:zhanghu-sos@qq.com', label: 'Email Me', highlight: true },
   ],
   // [OPTIONAL] Group your monitors
   // If not specified, all monitors will be shown in a single list
