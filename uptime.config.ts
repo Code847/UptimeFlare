122fe953a900e18d3f254497a7be3649d7f0b9bd
代号847 <42256643+Code847@users.noreply.github.com>
2026-03-23 16:17:57 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -288,7 +288,7 @@ const workerConfig: WorkerConfig = {
       // `method` should be `TCP_PING` for tcp monitors
       method: 'TCP_PING',
       // `target` should be `host:port` for tcp monitors
-      target: '144.76.97.102:10120',
+      target: 'BTMO.CN',
       tooltip: 'My production server SSH',
       statusPageLink: 'https://www.btmo.cn',
       timeout: 5000,
